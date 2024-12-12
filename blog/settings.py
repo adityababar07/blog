ebac9c91f44669894e036752e7c05f413b52e2bb
adityababar07 <adityababar715@gmail.com>
2024-12-12 23:32:32 +0530

    initial changes

--- blog/settings.py
+++ blog/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("staticfiles"))]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
