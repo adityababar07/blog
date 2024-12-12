60e6e406332a66433b928a7aea278b692ecfd122
adityababar07 <adityababar715@gmail.com>
2024-12-12 21:05:06 +0530

    initial changes

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install pipenv
+python3.9 -m pip3 install --upgrade pip
+python3.9 -m pip3 install pipenv
 python3.9 -m pipenv shell
 # Install dependencies
-pip install -r requirements.txt 
+pip3 install -r requirements.txt 
 
 # Run database migrations
 python3 manage.py makemigrations --noinput
